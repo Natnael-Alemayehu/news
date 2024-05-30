ce97af69e01774fcaaaa6408d803fda144fb2468
Natnael Alemayehu <se.natnael.alemayehu@gmail.com>
2024-05-30 17:05:03 +0300

    CRUD functionality completed!

---
+++ articles/admin.py
@@ -0,0 +1,14 @@
+from django.contrib import admin
+from articles.models import Article
+
+
+# Register your models here.
+class ArticleAdmin(admin.ModelAdmin):
+    list_display = [
+        "title",
+        "body",
+        "author",
+    ]
+
+
+admin.site.register(Article, ArticleAdmin)
